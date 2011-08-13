ec6a4abd0b0eb2ff3ea88f28a07eb0ba89ea22f9
Mark Engel <mark.c.engel@gmail.com>
2011-08-13 18:46:13 +0200

    put js to own file

---
+++ webroot/app.js
@@ -0,0 +1,61 @@
+var createUrl = function(bbox) {
+    var server = "http://dalino.spline.de/cgi-bin/proxy.cgi?url=http://xapi.spline.de/api/0.6/";
+    // transform from WGS 1984
+    // to Spherical Mercator Projection
+    bbox = bbox.transform(
+        new OpenLayers.Projection("EPSG:900913"),
+        new OpenLayers.Projection("EPSG:4326"));
+        var url = server + "node[amenity=pub][bbox=" +
+        bbox.left + "," + bbox.bottom + "," + bbox.right + "," + bbox.top + "]";
+        console.log(url);
+        return url;
+}
+
+var createLayer = function(extent, name) {
+    var layer = new OpenLayers.Layer.Vector(name, {
+        strategies: [new OpenLayers.Strategy.Fixed()],
+        protocol: new OpenLayers.Protocol.HTTP({
+            url: createUrl(extent),
+            format: new OpenLayers.Format.OSM()
+        }),
+        projection: new OpenLayers.Projection("EPSG:4326")
+    });
+    return layer;
+}
+
+var map;
+function init(){
+
+    map = new OpenLayers.Map("basicMap",{
+        controls:[
+            new OpenLayers.Control.Navigation(),
+            new OpenLayers.Control.PanZoomBar(),
+            new OpenLayers.Control.LayerSwitcher(),
+            new OpenLayers.Control.Attribution()],
+            maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
+            maxResolution: 156543.0399,
+            units: 'm'
+
+    } );
+    map.events.on({"moveend": function(event){
+        var layer = map.getLayersByName("Kneipen")[0];
+        if(layer) {
+            map.removeLayer(layer);
+            map.addLayer(createLayer(map.getExtent(),"Kneipen"));
+        }
+    } });
+    // add maps
+    var mapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+    map.addLayers([mapnik]);
+
+    // set startpoint
+    // transform from WGS 1984
+    // to Spherical Mercator Projection
+    var lonLat = new OpenLayers.LonLat(13.30,52.455)
+    .transform(
+        new OpenLayers.Projection("EPSG:4326"),
+        new OpenLayers.Projection("EPSG:900913"));
+
+        map.setCenter(lonLat,15);
+        map.addLayer(createLayer(map.getExtent(),"Kneipen"));
+}
